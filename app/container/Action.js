3bd2c4929a3f7a47dbac565604c7979ac2ce8e78
Benoit Deglane <deglane.benoit@gmail.com>
2016-05-03 20:32:01 +0200

    better

--- app/container/Action.js
+++ app/container/Action.js
@@ -1,27 +1,40 @@
 'use strict';
 export default class Action {
-    constructor() {
-    }
+	constructor() {
+	}
 
-    static water(state, id) {
-        if (state.fields[id].water < 100)
-            state.fields[id].water += 10;
+	static water(state, id) {
+		if (state.fields[id].water < 100)
+			state.fields[id].water += 10;
 
-        return state;
-    }
+		return state;
+	}
 
-    static harvest(state, id) {
-        if (state.fields[id].harvest >= 100)
-            state.fields[id].harvest = 0;
+	static harvest(state, id) {
+		if (state.fields[id].harvest >= 100) {
+			state.fields[id].harvest = 0;
+			state.fields[id].maturity = false;
+		}
 
-        return state;
-    }
+		return state;
+	}
 
-    static grow(state) {
-        for (let field in state.fields) {
-            state.fields[field].water -= 10;
-            state.fields[field].harvest += 10;
-        }
-        return state;
-    }
+	static grow(state) {
+		for (let field in state.fields) {
+			if (
+				state.fields[field].water > 0 &&
+				state.fields[field].harvest < 100
+			) {
+				state.fields[field].water -= 10;
+				state.fields[field].harvest += 10;
+			}
+
+			if (state.fields[field].harvest >= 100) {
+				state.fields[field].maturity = true;
+			} else {
+				state.fields[field].maturity = false;
+			}
+		}
+		return state;
+	}
 }
