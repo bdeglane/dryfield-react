f5bf9d1c262e120384255e434b7766895571b4e3
compas-tis <benoit.deglane@gmail.com>
2016-05-04 11:14:39 +0200

    Store  now an es6 class

--- app/core/Store.js
+++ app/core/Store.js
@@ -1,44 +1,69 @@
-/**
- *
- * @param reducer
- * @returns {{getState: getState, dispatch: dispatch, subscribe: subscribe}}
- */
-export const createStore = (reducer) => {
-    let state;
-    let listeners = [];
+'use strict';
 
-    /**
-     * @returns {state}
-     */
-    const getState = () => state;
-
+export default class Store {
     /**
      *
-     * @param action
-     * @param params
+     * @param reducer
      */
-    const dispatch = (action, params) => {
-        state = reducer(state, action, params);
-        listeners.forEach(listener => listener());
-    };
+    constructor(reducer) {
+        this.reducer = reducer;
+        // initial state of the application
+        // need immutability
+        this.state = {
+            score: 0,
+            waterTank: 100,
+            fields: [
+                {
+                    water: 100,
+                    harvest: 0,
+                    mature: false
+                }, {
+                    water: 100,
+                    harvest: 0,
+                    mature: false
+                }, {
+                    water: 100,
+                    harvest: 0,
+                    mature: false
+                }
+            ]
+        };
+        this.listeners = [];
+        // initial dispatch
+        // this.dispatch({});
+    }
 
     /**
+     * Retourne l'etat courant
+     *
+     * @returns {Object}
+     */
+    getState() {
+        return this.state;
+    }
+
+    /**
+     * Declare le declenchement d'une action et
+     * transmet eventuellement le(s) parametre(s)
+     *
+     * @param action string|const action name
+     * @param params *
+     */
+    dispatch(action, params) {
+        this.state = this.reducer(this.state, action, params);
+        this.listeners.forEach(listener => listener());
+    }
+
+    /**
+     * Un listener est une callback executer
+     * lors de l'appel d'une action
      *
      * @param listener
-     * @returns {Function}
      */
-    const subscribe = (listener)=> {
-        listeners.push(listener);
+    subscribe(listener) {
+        this.listeners.push(listener);
         return ()=> {
-            listeners = listeners.filter(l => l !== listener);
+            this.listeners = this.listeners.filter(l => l !== listener);
         }
-    };
-
-    /**
-     * first render
-     */
-    dispatch({});
-
-    return {getState, dispatch, subscribe};
-
-};
+    }
+}
