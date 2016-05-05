2970819689b44f85f159e6a0df8d5ae376e6c5d1
Benoit Deglane <deglane.benoit@gmail.com>
2016-05-05 11:03:52 +0200

    move reducer

--- app/container/Action.js
+++ app/container/Action.js
@@ -1,61 +1,100 @@
 'use strict';
 export default class Action {
-    constructor() {
-    }
+	/**
+	 *
+	 * @param state
+	 * @param action
+	 * @param params
+	 * @returns {*}
+	 */
+	static reducer(state, action, params) {
+		switch (action.type) {
+			case 'IRRIGUER':
+				return Action.irriguer(state, params);
+				break;
+			case 'RECOLTER':
+				return Action.recolter(state, params);
+				break;
+			case 'BUY_WATER':
+				return Action.buyWater(state);
+				break;
+			case 'GROW':
+				return Action.grow(state);
+				break;
+			default:
+				console.log('call unkown action current state return');
+				return state;
+				break;
+		}
+	}
 
-    static irriguer(state, id) {
-        if (state.fields[id].water < 100)
-            state.fields[id].water += 10;
+	static irriguer(state, id) {
+		if (state.game) {
+			if (state.fields[id].water < 100)
+				state.fields[id].water += 10;
+		}
+		return state;
+	}
 
-        return state;
-    }
+	static recolter(state, id) {
+		if (state.game) {
+			if (state.fields[id].harvest >= 100) {
+				state.fields[id].harvest = 0;
+				state.fields[id].increment *= 1.02;
+			}
+		}
+		return state;
+	}
 
-    static recolter(state, id) {
-        if (state.fields[id].harvest >= 100)
-            state.fields[id].harvest = 0;
+	static buyWater(state) {
+		if (state.game) {
+			if (state.score > 0) {
+				state.waterTank += 100;
+				state.score -= 1;
+			}
+		}
+		return state;
+	}
 
-        return state;
-    }
-
-    static buyWater(state) {
-        if (state.score > 0) {
-            state.waterTank += 100;
-            state.score -= 1;
-        }
-        return state;
-    }
-
-    static score(state) {
-        state.score += 1;
-        return state;
-    }
+	static score(state) {
+		if (state.game) {
+			state.score += 1;
+			return state;
+		}
+	}
 
 	static grow(state) {
-		for (let field in state.fields) {
-			// check maturity
-			if (state.fields[field].maturity = true) {
-				state.fields[field].timer -= 1;
-			}
+		if (state.game) {
+			for (let field in state.fields) {
 
-			// if timer ended, reset field
-			if (state.fields[field].timer <= 0) {
-				state.fields[field].harvest = 0;
-			}
+				// if timer ended
+				// reset field
+				if (state.fields[field].timer <= 0) {
+					state.fields[field].harvest = 0;
+					state.fields[field].timer = 4;
+					state.fields[field].maturity = false;
+				}
 
-			// increment
-			if (
-				state.fields[field].water > 0 &&
-				state.fields[field].harvest < 100
-			) {
-				state.fields[field].water -= 10;
-				state.fields[field].harvest += 10;
-			}
+				// check maturity
+				if (state.fields[field].maturity) {
+					state.fields[field].timer -= 1;
+				}
 
-			// check maturity
-			if (state.fields[field].harvest >= 100) {
-				state.fields[field].maturity = true;
-			} else {
-				state.fields[field].maturity = false;
+				// increment
+				if (
+					state.fields[field].water > 0 &&
+					state.fields[field].harvest < 100
+				) {
+					state.fields[field].water = Math.round((state.fields[field].water - state.fields[field].increment) * 100) / 100;
+					state.fields[field].harvest += 10;
+				}
+
+				// check maturity
+				if (state.fields[field].harvest >= 100) {
+					state.fields[field].maturity = true;
+				} else {
+					state.fields[field].maturity = false;
+				}
 			}
 		}
 		return state;
--- app/controller/Game.js
+++ app/controller/Game.js
@@ -1,77 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-// import {createStore} from '../core/_store.js';
-
 import Store from '../core/Store.js';
-
 import Action from '../container/Action.js';
 import DryField from '../container/DryField.jsx';
 
 
 export default class Game {
 
-    constructor() {
-        this.timer = null;
-        this.dump = 1;
-        this.store = new Store(this.reducer);
-    }
+	constructor() {
+		this.timer = null;
+		this.dump = 1;
+		this.store = new Store(Action.reducer);
+	}
 
-    init() {
-        this.store.subscribe(()=> this.render());
-        // this.frame();
-    }
+	init() {
+		this.store.subscribe(()=> this.render());
+		this.frame();
+	}
 
-    /**
-     *
-     * @param state
-     * @param action
-     * @param params
-     * @returns {*}
-     */
-    reducer(state, action, params) {
-        switch (action.type) {
-            case 'IRRIGUER':
-                return Action.irriguer(state, params);
-                break;
-            case 'RECOLTER':
-                return Action.recolter(state, params);
-                break;
-            case 'BUY_WATER':
-                return Action.buyWater(state);
-                break;
-            case 'GROW':
-                return Action.grow(state);
-                break;
-            default:
-                console.log('call unkown action current state return');
-                return state;
-                break;
-        }
-    }
+	frame() {
+		this.timer = setInterval(()=> {
+			this.store.dispatch({type: 'GROW'})
+		}, 1000);
 
-    frame() {
-        this.timer = setInterval(()=> {
-            this.store.dispatch({type: 'GROW'})
-        }, 1000);
+		// run for only 15 sec
+		setTimeout(()=> {
+			clearInterval(this.timer);
+			this.timer = null;
+		}, 30000);
+	}
 
-        // run for only 15 sec
-        setTimeout(()=> {
-            clearInterval(this.timer);
-            this.timer = null;
-        }, 15000);
-    }
-
-    render() {
-        console.log(this.store.getState());
-        ReactDOM.render(
-            <DryField
-                state={ this.store.getState() }
-                onIrriguer={ (params)=> this.store.dispatch({type:'IRRIGUER'},params) }
-                onRecolter={ (params)=> this.store.dispatch({type:'RECOLTER'},params) }
-                onBuyWater={ ()=> this.store.dispatch({type:'BUY_WATER'}) }
-            />,
-            document.getElementById('app')
-        );
-    }
+	render() {
+		console.log(this.store.getState());
+		ReactDOM.render(
+			<DryField
+				state={ this.store.getState() }
+				onIrriguer={ (params)=> this.store.dispatch({type:'IRRIGUER'},params) }
+				onRecolter={ (params)=> this.store.dispatch({type:'RECOLTER'},params) }
+				onBuyWater={ ()=> this.store.dispatch({type:'BUY_WATER'}) }
+			/>,
+			document.getElementById('app')
+		);
+	}
 }
